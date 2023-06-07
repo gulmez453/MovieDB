9ede2046f934cb5349f3298614a5b01eaa794842
Mansur74 <mansurcildir@hotmail.com>
2023-06-07 23:27:47 +0300

    Admin Skeleton

---
+++ MovieDB/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieDB.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public  IActionResult AddMovie()
+        {
+            return View();
+        }
+    }
+}
