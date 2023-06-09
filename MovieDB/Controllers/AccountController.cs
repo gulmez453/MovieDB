f33f4c41add8f2e69fdd3433d58035a5fc8ed06e
gulmez453 <93056318+gulmez453@users.noreply.github.com>
2023-06-10 00:22:07 +0300

    profil geliştirmeleri yapıldı

--- MovieDB/Controllers/AccountController.cs
+++ MovieDB/Controllers/AccountController.cs
@@ -2,10 +2,13 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
 using MovieDB.Entities;
 using MovieDB.Models;
 using NETCore.Encrypt.Extensions;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
 namespace MovieDB.Controllers
 {
@@ -35,17 +38,16 @@ namespace MovieDB.Controllers
         {
             if(ModelState.IsValid)
             {
-                string Salt = _configiration.GetValue<string>("AppSettings:Salt");
-                string saltedpass = model.Password + Salt;
-                string hashedPasword = saltedpass.MD5();
+                
+                string hashedPasword = SaltFunction(model.Password);
 
                 User user = _databaseContext.Users.SingleOrDefault(x => x.email.ToLower() == model.email.ToLower() && x.Password == hashedPasword);
                 if (user != null)
                 {
                     List<Claim> claims = new List<Claim>();
-                    claims.Add(new Claim("Id", user.Id.ToString()));
-                    claims.Add(new Claim("Name", user.FullName));
-                    claims.Add(new Claim(ClaimTypes.Role, user.Role));
+                    claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                    claims.Add(new Claim(ClaimTypes.Name, user.FullName?? string.Empty));
+                    claims.Add(new Claim(ClaimTypes.Role, user.Role?? string.Empty));
                     claims.Add(new Claim("email", user.email));
 
                     ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
@@ -76,16 +78,14 @@ namespace MovieDB.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (_databaseContext.Users.Any(x => x.email.ToLower()== model.email.ToLower()))
+                if (_databaseContext.Users.Any(x => x.email.ToLower() == model.email.ToLower()))
                 {
                     ModelState.AddModelError(nameof(model.email), "Email is already exists.");
                     return View(model);
                 }
-                string Salt = _configiration.GetValue<string>("AppSettings:Salt");
-                string saltedpass = model.Password + Salt;
-                string hashedPasword = saltedpass.MD5();
+                string hashedPasword = SaltFunction(model.Password);
 
-                User user = new ()
+                User user = new()
                 {
                     email = model.email,
                     Password = hashedPasword,
@@ -93,10 +93,10 @@ namespace MovieDB.Controllers
                 };
 
                 _databaseContext.Users.Add(user);
-                
+
                 int affectedRowCount = _databaseContext.SaveChanges();
 
-                if(affectedRowCount == 0)
+                if (affectedRowCount == 0)
                 {
                     ModelState.AddModelError("", "User can not be added");
                 }
@@ -110,10 +110,86 @@ namespace MovieDB.Controllers
             return View(model);
            
         }
+
+        private string SaltFunction(string password)
+        {
+            string Salt = _configiration.GetValue<string>("AppSettings:Salt");
+            string saltedpass = password + Salt;
+            string hashedPasword = saltedpass.MD5();
+            return hashedPasword;
+        }
+
         public IActionResult Profile()
         {
+            ProfileLoader();
+
             return View();
         }
+
+        private void ProfileLoader()
+        {
+            Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            User user = _databaseContext.Users.SingleOrDefault(a => a.Id == userid);
+
+            ViewData["email"] = user.email;
+            ViewData["FullName"] = user.FullName;
+        }
+
+        [HttpPost]
+        public IActionResult ProfileChangeEmail([Required][StringLength(30)] string? email)
+        {
+            if(ModelState.IsValid)
+            {
+                Guid userid = new Guid (User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.SingleOrDefault ( a => a.Id == userid);
+
+                user.email = email;
+                _databaseContext.SaveChanges();
+                ViewData["result"] = "EmailChanged";
+              
+                
+            }
+            ProfileLoader();
+            return View("Profile");
+        }
+
+        [HttpPost]
+        public IActionResult ProfileChangeName([Required][StringLength(30)] string? name)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.SingleOrDefault(a => a.Id == userid);
+
+                user.FullName = name;
+                _databaseContext.SaveChanges();
+                ViewData["result"] = "NameChanged";
+                
+
+            }
+            ProfileLoader();
+            return View("Profile");
+        }
+
+        [HttpPost]
+        public IActionResult ProfileChangePassword([Required][MinLength(6)][MaxLength(16)] string? password)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.SingleOrDefault(a => a.Id == userid);
+
+                string hashedPassword = SaltFunction(password);
+
+                user.Password = hashedPassword;
+                _databaseContext.SaveChanges();
+
+                ViewData["result"] = "PasswordChanged";
+            }
+            ProfileLoader();
+            return View("Profile");
+        }
+
         public IActionResult Logout()
         {
             HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
