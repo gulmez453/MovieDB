3d20b339d0cc2832693d1e65e4be26ba663414cc
Mansur74 <mansurcildir@hotmail.com>
2023-06-09 14:30:32 +0300

    Add Movie DB

--- MovieDB/Controllers/AdminController.cs
+++ MovieDB/Controllers/AdminController.cs
@@ -1,15 +1,64 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieDB.Entities;
+using MovieDB.Models;
+using NETCore.Encrypt.Extensions;
+using NuGet.Protocol.Plugins;
 
 namespace MovieDB.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly DatabaseContext _databaseContext;
+        public AdminController(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public  IActionResult AddMovie()
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  IActionResult AddMovie(MovieViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_databaseContext.Movies.Any(movie => movie.Title.ToLower() == model.Title.ToLower()))
+                {
+                    ModelState.AddModelError(nameof(model.Title), "The movie is already exists.");
+                    return View(model);
+                }
+
+                Movie movie = new Movie()
+                {
+                    Title = model.Title,
+                    Producer = model.Producer,
+                    Director = model.Director,
+                    MusicDirector = model.MusicDirector,
+                    ProduceIn = model.ProduceIn,
+                };
+
+                _databaseContext.Movies.Add(movie);
+
+                int affectedRowCount = _databaseContext.SaveChanges();
+
+                if (affectedRowCount == 0)
+                {
+                    ModelState.AddModelError("", "The movie can not be added");
+                }
+
+            }
+
+            return View(model);
+        }
+
+        public IActionResult UpdateMovie()
         {
             return View();
         }
---
+++ MovieDB/Entities/Movie.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieDB.Entities
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [StringLength(30)]
+        [Required]
+        public string Title { get; set; }
+
+        [StringLength(30)]
+        [Required]
+        public string Producer { get; set; }
+
+        [StringLength(30)]
+        [Required]
+        public string Director { get; set; }
+
+        [StringLength(30)]
+        [Required]
+        public string MusicDirector { get; set; }
+
+        [StringLength(30)]
+        [Required]
+        public string ProduceIn { get; set; }
+
+    }
+}
+
