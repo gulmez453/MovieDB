778405d9e0d8713365c256fa0acf2a68e4e5ba35
ENES GÜLER <18050111005@ybu.edu.tr>
2023-06-17 13:52:39 +0300

    New View added to be able to filter movies

---
+++ MovieDB/Models/FilterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieDB.Models
+{
+    public class FilterViewModel
+    {
+        public string Search { get; set; }
+        public string Category { get; set; }
+        public string ProduceYearMin { get; set; }
+        public string ProduceYearMax { get; set; }
+        public string MinuteMin { get; set; }
+        public string MinuteMax { get; set; }
+        public bool Rate1 { get; set; }
+        public bool Rate2 { get; set; }
+        public bool Rate3 { get; set; }
+        public bool Rate4 { get; set; }
+        public bool Rate5 { get; set; }
+
+
+    }
+}
---
+++ MovieDB/Models/MovieFilterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieDB.Models
+{
+    public class MovieFilterViewModel
+    {
+        public FilterViewModel FilterViewModell { get; set; }
+        public List<MovieDB.Entities.Movie> MovieViewModel { get; set; }
+
+    }
+}
