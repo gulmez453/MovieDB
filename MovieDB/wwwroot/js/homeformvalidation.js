86cf6add5f7d92dca4a668e8fe3e50c2e7e81d6e
enesguler <18050111005@ybu.edu.tr>
2023-06-18 21:01:22 +0300

    form validation js code added

---
+++ MovieDB/wwwroot/js/homeformvalidation.js
@@ -0,0 +1,18 @@
+﻿function validateInput(inputId) {
+    const textInput = document.getElementById(inputId);
+    const submitButton = document.getElementById('filter-submit-button');
+    const inputValue = textInput.value.trim();
+
+    // Remove any non-digit characters from the input
+    const numericInput = inputValue.replace(/\D/g, '');
+
+    // Update the input field with the cleaned numeric value
+    textInput.value = numericInput;
+
+    // Enable or disable the submit button based on the input validity
+    if (numericInput.length > 0) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+}
