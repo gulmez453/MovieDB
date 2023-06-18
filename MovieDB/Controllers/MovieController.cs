f48727fcedfc8583801c07eeb3ac29ffe5dacc19
Mansur74 <mansurcildir@hotmail.com>
2023-06-18 10:17:17 +0300

    Update Rate operations

--- MovieDB/Controllers/MovieController.cs
+++ MovieDB/Controllers/MovieController.cs
@@ -179,7 +179,7 @@ namespace MovieDB.Controllers
             _databaseContext.Add(comment);
             _databaseContext.SaveChanges();
 
-            return RedirectToAction("Details", new { movieId = movieId });
+            return RedirectToAction("Index");
 
             
         }
