9f1dbe7d4ed5303bc13e60667b00b0b295467a3f
enesguler <18050111005@ybu.edu.tr>
2023-06-18 21:11:25 +0300

    bugfix

--- MovieDB/wwwroot/js/homeformvalidation.js
+++ MovieDB/wwwroot/js/homeformvalidation.js
@@ -1,6 +1,5 @@
 ﻿function validateInput(inputId) {
     const textInput = document.getElementById(inputId);
-    const submitButton = document.getElementById('filter-submit-button');
     const inputValue = textInput.value.trim();
 
     // Remove any non-digit characters from the input
@@ -8,11 +7,4 @@
 
     // Update the input field with the cleaned numeric value
     textInput.value = numericInput;
-
-    // Enable or disable the submit button based on the input validity
-    if (numericInput.length > 0) {
-        submitButton.disabled = false;
-    } else {
-        submitButton.disabled = true;
-    }
 }
